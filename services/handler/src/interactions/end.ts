ff22498093016bf14deba8b83675aafb3801da1b
didinele <didinele.dev@gmail.com>
2022-08-09 22:58:49 +0300

    chore: remove old code

--- services/handler/src/interactions/end.ts
+++
@@ -1,4 +0,0 @@
-export const EndCommand = {
-  name: 'end',
-  description: 'Ends the current AMA session'
-} as const;
--- services/handler/src/util/Constants.ts
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-  APPROVED: 6931610,
-  DENIED: 15953004,
-  FLAGGED: 15309853,
-  BLURPLE: 7506394
-} as const;
-
-Object.freeze(COLORS);
