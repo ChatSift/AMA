7d15a3e7ddb5b2d31a5588a01aa15d0914b40777
Sazzo <felipe@sazz.dev>
2022-10-16 14:07:26 -0300

    feat(api): add get amas endpoint

---
+++ packages/api/src/routes/getAMAs.ts
@@ -0,0 +1,30 @@
+import { Route, RouteMethod } from '@chatsift/rest-utils';
+import { PrismaClient } from '@prisma/client';
+import type { Middleware, Request, Response } from 'polka';
+import { singleton } from 'tsyringe';
+import type { Ama } from '../util/models';
+
+@singleton()
+export default class extends Route<Ama[], never> {
+	public info = {
+		method: RouteMethod.get,
+		path: '/ama/v1/guilds/:guildId/amas/',
+	} as const;
+
+	public constructor(private readonly prisma: PrismaClient) {
+		super();
+	}
+
+	public async handle(req: Request, res: Response) {
+		const { guildId } = req.params as { guildId: string };
+		const amas = await this.prisma.ama.findMany({
+			where: {
+				guildId,
+			},
+		});
+
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(amas));
+	}
+}
---
+++ packages/api/src/routes/index.ts
@@ -0,0 +1,1 @@
+export { default as GetAMAs } from './getAMAs';
